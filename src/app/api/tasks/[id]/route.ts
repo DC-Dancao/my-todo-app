48fdb923d2430f838544d81b6d28e0f2d0c99c60
淡草 <47478484+DC-Dancao@users.noreply.github.com>
2025-08-21 15:16:20 +0800

    fix type errors

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -3,15 +3,15 @@ import { tasks } from '@/db/schema';
 import { eq } from 'drizzle-orm';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function PUT(request: NextRequest, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
   const { title, description, completed } = await request.json();
   const updatedTask = await db.update(tasks).set({ title, description, completed, updatedAt: new Date() }).where(eq(tasks.id, parseInt(id))).returning();
   return NextResponse.json(updatedTask[0]);
 }
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function DELETE(request: NextRequest, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
   const deletedTask = await db.delete(tasks).where(eq(tasks.id, parseInt(id))).returning();
   return NextResponse.json(deletedTask[0]);
 }
